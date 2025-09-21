dc28e7121b6a9a2ab8217f1c4a55bc95402a7183
harisankar-sh <hari983@icloud.com>
2025-09-21 20:02:55 +0400

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,20 +1,15 @@
 AUTHOR = 'harisankar'
 SITENAME = 'harisankar'
 SITEURL = ""
-
 PATH = "content"
-
 TIMEZONE = 'Asia/Dubai'
-
 DEFAULT_LANG = 'en'
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
 # Blogroll
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
@@ -22,14 +17,11 @@ LINKS = (
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
     ("You can modify those links in your config file", "#"),
 )
-
 # Social widget
 SOCIAL = (
     ("You can add links in your config file", "#"),
     ("Another social link", "#"),
 )
-
 DEFAULT_PAGINATION = False
-
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
